8f24a5df1db28f57c439d6f2ad9b2b208f53168c
JulesChabaud <j.chabaud@codeur.online>
2020-05-29 11:42:48 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <title>Projet integration n°2</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link rel="stylesheet" href="main.css">
+  </head>
+  <body>
+
+<header>
+
+<div class="background-bloc" style="height:150px">
+
+</div>
+
+<div class="container-fluid" id="navbar-header">
+  <div class="row" style="height: 150px">
+    <div class="col text-center my-auto">
+      BLUEASY
+    </div>
+    <div class="col-6 text-center my-auto">
+                <nav class="navbar navbar-expand-lg navbar-dark contenu-nav">
+                  <button class="btn btn-outline-info my-2 my-sm-0" type="submit">HOME</button>
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                      <a class="nav-item nav-link" href="#">SERVICES</a>
+                      <a class="nav-item nav-link" href="#">FEATURES</a>
+                      <a class="nav-item nav-link" href="#">PORTFOLIO</a>
+                      <a class="nav-item nav-link" href="#">CONTACT</a>
+                    </div>
+                  </div>
+        </nav>
+    </div>
+  </div>
+</div>
+
+
+</header>
---
+++ index.php
@@ -0,0 +1,156 @@
+<?php include 'header.php'; ?>
+
+<div class="bg-hero">
+  <div class="d-flex align-items-center flex-column justify-content-center text-white h-100" id="text-overlay">
+    <h2 class="display-4 highlight-text">"I'M LOOKING FOR THE UNEXPECTED.</h2>
+
+    <div class="w-100"></div>
+
+    <h2 class="display-4 highlight-text">I'M LOOKING FOR THINGS I'VE NEVER SEEN BEFORE"</h2>
+  </div>
+</div>
+
+<div class="container-services">
+
+  <div class="container d-flex align-items-center flex-column justify-content-center text-white h-100">
+  <div class="row">
+    <div class="col-sm">
+      <div class="card border-0" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+    </div>
+    <div class="col-sm">
+      <div class="card border-0" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+    </div>
+    <div class="col-sm">
+      <div class="card border-0" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+    </div>
+    <div class="col-sm">
+      <div class="card border-0" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+
+</div>
+
+<div class="container-pf">
+
+  <div class="container-fluid" id="navbar-pf">
+    <div class="row" style="height: 150px">
+      <div class="col text-center my-auto">
+        BLUEASY
+      </div>
+      <div class="col-6 text-center my-auto">
+                  <nav class="navbar navbar-expand-lg navbar-dark contenu-nav">
+                    <button class="btn btn-outline-info my-2 my-sm-0" type="submit">HOME</button>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                      <div class="navbar-nav">
+                        <a class="nav-item nav-link text-dark" href="#">SERVICES</a>
+                        <a class="nav-item nav-link text-dark" href="#">FEATURES</a>
+                        <a class="nav-item nav-link text-dark" href="#">PORTFOLIO</a>
+                        <a class="nav-item nav-link text-dark" href="#">CONTACT</a>
+                      </div>
+                    </div>
+          </nav>
+      </div>
+    </div>
+  </div>
+
+  <div class="container d-flex align-items-center flex-column justify-content-center text-white h-100">
+  <div class="row">
+    <div class="col">
+      <img src="..." class="img-fluid" alt="Responsive image">
+    </div>
+    <div class="col">
+      <img src="..." class="img-fluid" alt="Responsive image">
+    </div>
+    <div class="col">
+      <img src="..." class="img-fluid" alt="Responsive image">
+    </div>
+    <div class="w-100"></div>
+    <div class="col">
+      <img src="..." class="img-fluid" alt="Responsive image">
+    </div>
+    <div class="col">
+      <img src="..." class="img-fluid" alt="Responsive image">
+    </div>
+    <div class="col">
+      <img src="..." class="img-fluid" alt="Responsive image">
+    </div>
+  </div>
+</div>
+
+</div>
+
+<div class="container-section">
+
+  <div class="container">
+    <div class="row">
+      <div class="col-sm">
+        <div class="jumbotron jumbo-section">
+          <h1 class="display-4">Hello, world!</h1>
+          <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <hr class="my-4">
+          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+          <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </div>
+      </div>
+      <div class="col-sm">
+        <div class="embed-responsive embed-responsive-16by9">
+          <iframe class="embed-responsive-item" src="https://www.youtube.com/watch?v=PnaDq-T3Z68" allowfullscreen></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+<div class="container-twitter">
+
+  <div class="jumbotron jumbotron-fluid jumbo-twitter">
+    <div class="container">
+      <h1 class="display-4 text-light">Fluid jumbotron</h1>
+      <p class="lead text-light">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+    </div>
+  </div>
+
+</div>
+
+<div class="container-who">
+
+  <div class="jumbotron jumbotron-fluid jumbo-who">
+    <div class="container">
+      <h1 class="display-4 text-light">Fluid jumbotron</h1>
+      <p class="lead text-light">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+    </div>
+  </div>
+
+</div>
+
+
+<?php include 'footer.php'; ?>
